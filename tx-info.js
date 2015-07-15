b28a3667a6a9b4e91320642a2f053b6d99901819
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-07-15 12:23:18 -0400

    return tx info even if no data is found

--- tx-info.js
+++ tx-info.js
@@ -5,28 +5,39 @@ var TxData = require('./tx-data')
 var DATA_TYPES = TxData.types
 
 module.exports = function getTxInfo(tx, networkName, prefix) {
-  var txData = TxData.fromTx(tx, prefix)
-  if (!txData) return
-
   var addresses = {}
-  addresses.from = tx.ins.map(function(i) {
-      return utils.getAddressFromInput(i, networkName)
-    })
-    .filter(truthy)
-
-  addresses.to = tx.outs.map(function(o) {
-      return utils.getAddressFromOutput(o, networkName)
-    }).filter(truthy)
-
-  return {
-    type: txData.type() === DATA_TYPES.public ? 'public' : 'permission',
-    key: txData.data(),
+  var parsed = {
     tx: {
       id: tx.getId(),
       body: tx,
       addresses: addresses
     }
   }
+
+  addresses.from = tx.ins
+    .map(function(i) {
+      return utils.getAddressFromInput(i, networkName)
+    })
+    .filter(truthy)
+
+  addresses.to = tx.outs
+    .map(function(o) {
+      return utils.getAddressFromOutput(o, networkName)
+    })
+    .filter(truthy)
+
+  var txData = TxData.fromTx(tx, prefix)
+  if (txData) {
+    parsed.type = txData.type() === DATA_TYPES.public ? 'public' : 'permission'
+    parsed.key = txData.data()
+  }
+
+  parsed.data = {
+    type: txData.type() === DATA_TYPES.public ? 'public' : 'permission',
+    key: txData.data(),
+  }
+
+  return parsed
 }
 
 function truthy (o) {
